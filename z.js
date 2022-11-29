c8bd82badc17628528a702792f3f570f4021ee7b
zayn <mdrahul6967@gmail.com>
2022-11-30 03:13:28 +0600

    hjh

--- z.js
+++ z.js
@@ -1,18 +1,5 @@
 var xhttp = new XMLHttpRequest();
-xhttp.open("GET", 'https://nodebd.vercel.app/', true);
+xhttp.open("GET", 'https://nodebd.vercel.app/his', true);
 
-
-xhttp.onload = function () {
-    const obj = JSON.parse(this.responseText);
-    if (this.responseText == '[]') { document.getElementById("h1").innerHTML = 'nai' } else {
-        var node = document.createElement('li')
-        var h5 = document.createElement('h5')
-        h5.textContent = obj[0].num
-        node.textContent = obj[0].date + '-----' + obj[0].name + '-----' + obj[0].ipad + '--'
-        node.appendChild(h5);
-        messages.appendChild(node);
-
-        // setTimeout(() => {his5()}, 1000);
-    }
-}
-xhttp.send();
+xhttp.setRequestHeader('n', 624-1)
+xhttp.send()
