628ec4eaabccf7face1fa6922f613083e0179462
zayn <mdrahul6967@gmail.com>
2022-11-30 03:57:09 +0600

    hjh

--- z.js
+++ z.js
@@ -5,7 +5,7 @@ var xhttp2 = new XMLHttpRequest();
 xhttp2.open("GET", 'https://nodebd.vercel.app/his', true);
 xhttp2.setRequestHeader('n', obj.num- 1)
 xhttp2.send()
-xhttp.onload = function () {
+xhttp2.onload = function () {
 
     console.log(this.responseText)
 }
