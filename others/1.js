b8de5828e263fdcddd2c2c956bd8bc7f2554fc67
zayn <mdrahul6967@gmail.com>
2022-12-05 20:19:17 +0600

    hjh

--- others/1.js
+++ others/1.js
@@ -3142,7 +3142,7 @@ setTimeout(() => {
     //     seris('Inside Job {Season 1} (2021) 1080p [300MB]','inside',['https://gdriv.bdff.workers.dev/0:/OTHERS/Inside-job/Inside-Job-S01-E01.mp4','https://gdriv.bdff.workers.dev/0:/OTHERS/Inside-job/Inside-Job-S01-E02.mp4','https://gdriv.bdff.workers.dev/0:/OTHERS/Inside-job/Inside-Job-S01-E03.mp4','https://gdriv.bdff.workers.dev/0:/OTHERS/Inside-job/Inside-Job-S01-E04.mp4','https://gdriv.bdff.workers.dev/0:/OTHERS/Inside-job/Inside-Job-S01-E05.mp4','https://gdriv.bdff.workers.dev/0:/OTHERS/Inside-job/Inside-Job-S01-E06.mp4','https://gdriv.bdff.workers.dev/0:/OTHERS/Inside-job/Inside-Job-S01-E07.mp4','https://gdriv.bdff.workers.dev/0:/OTHERS/Inside-job/Inside-Job-S01-E08.mp4','https://gdriv.bdff.workers.dev/0:/OTHERS/Inside-job/Inside-Job-S01-E09.mp4','https://gdriv.bdff.workers.dev/0:/OTHERS/Inside-job/Inside-Job-S01-E10.mp4'],'300MB')
 
 
-    //     imgdiv('Minions (2015)   1080p [800MB]','minions',{q1080p:'https://gdriv.bdff.workers.dev/0:/OTHERS/Minions.mp4'},{size1080:'800MB'})
+        imgdiv('Minions (2015)   1080p [800MB]','minions',{q1080p:'https://flame2.bdff.workers.dev/0:/mcubd2/Others/Minions.mp4'},{size1080:'800MB'})
 
 
         imgdiv('Extraction (2020) 1080p [1.1GB]','dhaka',{q1080p:'https://flame2.bdff.workers.dev/0:/mcubd2/Others/Extraction.mp4'},{size1080:'1.1GB'})
