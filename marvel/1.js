0f3a1ca336e4e1f278dfa42d24c44683646b64c7
zayn <mdrahul6967@gmail.com>
2023-01-01 03:33:42 +0600

    hjh

--- marvel/1.js
+++ marvel/1.js
@@ -2895,12 +2895,12 @@ setTimeout(() => {
     // imgdiv(name,img,q7,size,midcredit,pc)
 
 
-    imgdiv('Deadpool [1.1GB]', 'pool', ['https://root.bdff.workers.dev/0:/mcubd/Marvel/Deadpool.mp4'], '1.1GB')
+    seris('Deadpool [1.1GB]', 'pool', ['https://root.bdff.workers.dev/0:/mcubd/Marvel/Deadpool.mp4'], '1.1GB')
 
-    imgdiv('Venom: Let There Be Carnage [925MB]','venom2',['https://root.bdff.workers.dev/0:/mcubd/Marvel/venom2.mp4'],'925MB')
+    seris('Venom: Let There Be Carnage [925MB]','venom2',['https://root.bdff.workers.dev/0:/mcubd/Marvel/venom2.mp4'],'925MB')
 
 
-    imgdiv('Deadpool 2 [1.2GB]','pool2',['https://root.bdff.workers.dev/0:/mcubd/Marvel/deadpool-2.mp4'],'1.2GB')
+    seris('Deadpool 2 [1.2GB]','pool2',['https://root.bdff.workers.dev/0:/mcubd/Marvel/deadpool-2.mp4'],'1.2GB')
 
 
 
